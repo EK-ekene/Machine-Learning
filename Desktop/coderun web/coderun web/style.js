82acfbc386cbd4068f262c92aa1ffd786cf0b3af
EK-ekene <oppongedward02@gmail.com>
2025-01-21 10:53:49 +0000

    CodeRun

---
+++ Desktop/coderun web/coderun web/style.js
@@ -0,0 +1,11 @@
+
+function bar() {
+    var s=document.getElementById("aside");
+    if (s.style.display === "block") {
+        s.style.display = "none";
+    } else {
+        s.style.display= "block";
+    }
+}
+
+
